83a649a4963381c8743b07e4567b604397094a5f
Ad-Astraa <lrnddiniz@gmail.com>
2021-06-12 12:02:05 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }  //Code first, id como chave primária
+        public string Descricao { get; set; }
+
+    }
+}
---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext  //Context herda de DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; } //Tenho uma tabela chamada categorias | Basta referenciar o Context para acessá-lo
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //Método de configuração do Entity, nos informa qual banco de dados usaremos
+        {
+            //String de conexão
+            optionsBuilder.UseSqlServer(connectionString: @"Server = (localdb)\mssqllocaldb);Database = CursoMVC;Integrated Security = True"); //Parâmetro opcional da string de conexão
+        }
+    }
+}
